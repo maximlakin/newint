ccc73f168049747afcfc793f0f79e9eba94b7f84
Max Lakin <axml360@gmail.com>
2013-01-15 16:18:33 -0800

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newint::Application.config.secret_token = '1a41f0dd5086bbea06c2f6069f7f339d17f9966d1de89e3ed44b1f8e7fdf2f74526b1e243e09e16353b3d6e8d2a952e9dcaa43ce0f4e876caf96efba3924a2c0'
